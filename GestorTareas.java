17b7786c70e5119aabb597aa8767528caf2af14c
Patricia <p.santamartamedina@gmail.com>
2021-03-04 09:31:03 +0100

    primer git

---
+++ GestorTareas.java
@@ -0,0 +1,18 @@
+
+
+public class GestorTareas{
+
+    private int x;
+
+    /**
+     * Constructor for objects of class GestorTareas
+     */
+    public GestorTareas(){
+        x = 0;
+    }
+
+    public int sampleMethod(int y)
+    {
+        return x + y;
+    }
+}
